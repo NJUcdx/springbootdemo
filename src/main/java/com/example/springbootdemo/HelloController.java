945dca697ed9d7ae99988e2efaa506a034e9a183
chendongxiao <MF21320014@smail.nju.edu.cn>
2022-01-26 17:16:12 +0800

    test hook

--- src/main/java/com/example/springbootdemo/HelloController.java
+++ src/main/java/com/example/springbootdemo/HelloController.java
@@ -8,7 +8,7 @@ public class HelloController {
 
     @GetMapping("/hello")
     public String hello(){
-        return "hello,world,how are you?";
+        return "hello world, how are you?";
     }
 
 }
