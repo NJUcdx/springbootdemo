96937579ea7bd3964fd822946a206dd9a4c61f35
chendongxiao <MF21320014@smail.nju.edu.cn>
2022-01-27 14:01:08 +0800

    update sonarqube jacoco

--- src/test/java/com/example/springbootdemo/SpringbootdemoApplicationTests.java
+++ src/test/java/com/example/springbootdemo/SpringbootdemoApplicationTests.java
@@ -1,13 +1,26 @@
 package com.example.springbootdemo;
 
+import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
 @SpringBootTest
 class SpringbootdemoApplicationTests {
 
+    @Autowired
+    HelloController helloController;
+
     @Test
     void contextLoads() {
+    }
+
+    @Test
+    public void hello() {
+        String res = helloController.hello();
+        String expected = "hello world, fuck you !";
+        Assertions.assertEquals(expected, res);
     }
 
 }
